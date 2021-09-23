ed5086d0c004ccd07205a3b62ae229d6697601c7
sunbalabro <sunbalabro786@gmail.com>
2021-09-23 14:54:46 +0500

    assignment is complete

---
+++ app.js
@@ -0,0 +1,81 @@
+// Task No 1
+
+//write a program which shows the couter after each second.
+
+//-----------------------------------------------------------
+
+// var counter = 0;
+// setInterval(() => {
+//     counter++
+//     console.log(counter)
+// }, 1000);
+
+//----------------------------------------------------------
+
+// Task No 2
+
+// write a function which takes your name and displays the greeting with your name
+
+//----------------------------------------------------------
+
+// const greetings=()=> {
+//     const userName = prompt("What's your name");
+//     alert(`Welcome  ${userName}`)
+// }
+// greetings()
+
+//---------------------------------------------------------------
+
+// Task No 3
+
+// write a function(arrow function) which takes two values and return its sum as a result
+
+//---------------------------------------------------------------
+
+// const Sum = () =>console.log(2+2)   
+// Sum()
+
+//----------------------------------------------------------------
+
+// Task No 4
+
+
+//write a function(arrow function) which takes a number and multiply it with 0.5 and return the new value. print the new value outside the function
+
+//--------------------------------------------------------------------------
+
+// const sumOFValue = () =>{
+//    let num = 5;
+//    return num*0.5
+// }
+// console.log(sumOFValue())
+
+//-------------------------------------------------------------------------
+
+// Task No 5
+
+//print simple array of [1,2,3,4,5] with the help of array map funtion
+
+//-----------------------------------------------------------------------
+
+// var numArr = [1,2,3,4,5];
+// numArr.map((num)=>{
+//   console.log(num)
+// })
+
+//-------------------------------------------------------------------------
+
+// Task No 6
+
+// let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}]
+// iterate the given array through map function and print the name and id
+
+//--------------------------------------------------------------------------
+
+// let arr = [{id:0,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}]
+
+// arr.map((user)=>{
+//    console.log(`Name ====>${user.name} Id =====> ${user.id}`)
+// })
+
+//---------------------------------------------------------------------------
